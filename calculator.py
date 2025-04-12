6957f2f375fedfde303df0b1d16dd52a7e1f04ea
Ahmet Melih Çalış <sengmelihcalis@gmail.com>
2025-04-12 22:46:48 +0300

    İlk commit: Toplama ve çıkarma eklendi

---
+++ calculator.py
@@ -0,0 +1,15 @@
+def topla(a, b):
+    return a + b
+
+def cikar(a, b):
+    return a - b
+
+def main():
+    print(" ***HESAP MAKİNESİ***")
+    sayi1 = float(input("1.Sayıyı Giriniz: "))
+    sayi2 = float(input("2.Sayıyı Giriniz: "))
+    print(f"Toplam: {topla(sayi1, sayi2)}")
+    print(f"Fark: {cikar(sayi1, sayi2)}")
+
+if __name__ == "__main__":
+    main()
