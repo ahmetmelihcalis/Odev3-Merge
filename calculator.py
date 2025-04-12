3a510cacd57d3e5eb4c548aefdb15bd907bce961
Ahmet Melih Çalış <sengmelihcalis@gmail.com>
2025-04-12 23:32:16 +0300

    Çarpma özelliği eklendi

--- calculator.py
+++ calculator.py
@@ -4,12 +4,16 @@ def topla(a, b):
 def cikar(a, b):
     return a - b
 
+def carp(a, b):
+    return a * b
+
 def main():
     print(" ***HESAP MAKİNESİ***")
     sayi1 = float(input("1.Sayıyı Giriniz: "))
     sayi2 = float(input("2.Sayıyı Giriniz: "))
     print(f"Toplam: {topla(sayi1, sayi2)}")
     print(f"Fark: {cikar(sayi1, sayi2)}")
+    print(f"Çarpım: {carp(sayi1, sayi2)}")
 
 if __name__ == "__main__":
     main()
